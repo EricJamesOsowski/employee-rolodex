dd18fcce7faf7a1d859edfe7492e6a7532ffa137
Eric Osowski <ericosowski@Erics-MacBook-Pro.local>
2020-11-18 20:53:40 -0600

    Pulling data from randomUsers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import Header from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import API from "./utils/API";
+
+
+
 
 function App() {
+  const [developerState, setDeveloperState] = useState({
+    firstName: "",
+    lastName: "",
+    imageURL: "",
+    phoneNumber: ""
+  });
+
+  useEffect(() => {
+    // For demonstration purposes, we mock an API call.
+    API.getEmployees().then((res) => {
+      setDeveloperState(res.data);
+      console.log("RES DOT DATA DOT RESULTS");
+      console.log(res.data.results[1]);
+      console.log("Developer State:");
+      console.log(developerState);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
+    <div className="card">
+      <div>
+        Name:
+      </div>
+      <div>
+        Status:
+      </div>
+      <div>
+        Lifelong learner:
+      </div>
+      <div>
+        Excitement Level: 
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+  
+import axios from "axios";
+
+// Export an object with a "search" method that searches for random users for the passed query
+export default {
+  getEmployees: function() {
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  }
+};
