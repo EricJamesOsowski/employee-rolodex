79dcaedb3416945280afc907d2791ba2f522549b
Eric Osowski <ericosowski@Erics-MacBook-Pro.local>
2020-11-19 23:36:17 -0600

    Search and Sort functionality working

--- src/components/table.js
+++ src/components/table.js
@@ -6,13 +6,14 @@ function Table() {
   const [employees, setEmployees] = useState([]);
   const [searchTerm, setSearchTerm] = React.useState("");
   const [searchResults, setSearchResults] = React.useState([]);
+  const [searchFlags, setSearchFlags] = useState({ "name.first": true, "email": true });
 
   const handleChange = event => {
-     setSearchTerm(event.target.value);
-   };
+    setSearchTerm(event.target.value);
+  };
 
 
-   // This is where teh employee data is being populated
+  // This is HOW the employee data is being populated
   const loadData = () => {
     API.getEmployees()
       .then(response => response.json())
@@ -22,9 +23,11 @@ function Table() {
       })
   }
 
+  // This is WHERE the employees get populated
+  // Re renders the table any time the search term changes, 
   const isInitialMount = React.useRef(true);
   useEffect(() => {
-    if (isInitialMount.current){
+    if (isInitialMount.current) {
       loadData();
       isInitialMount.current = false;
     }
@@ -35,8 +38,10 @@ function Table() {
     setSearchResults(results);
   }, [searchTerm]);
 
-  let sortedEmployees = [...employees];
-  sortedEmployees.sort((a, b) => {
+
+  // Sorts employees by first name ascending
+  let employeesSortedByName = [...employees];
+  employeesSortedByName.sort((a, b) => {
     if (a.name.first < b.name.first) {
       return -1;
     }
@@ -46,20 +51,59 @@ function Table() {
     return 0;
   });
 
-  return (
-    <div>
-          <input
-        type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={handleChange}
-      />
+ function sortEmployeesBy(searchParameter){
+   let searchValueA;
+   let searchValueB;
+    let sortedSearchResults = [...searchResults]
+    let sortDirection = true;
+    sortedSearchResults.sort((a, b) => {
+
+      if (searchParameter == "name.first")
+      {
+        searchValueA = a.name.first;
+        searchValueB = b.name.first;
+        setSearchFlags(searchFlags => ({ ...searchFlags, "name.first": !searchFlags["name.first"]}))
+        sortDirection = searchFlags["name.first"];
+      }
+      else if (searchParameter == "email")
+      {
+        searchValueA = a.email;
+        searchValueB = b.email;
+        setSearchFlags(searchFlags => ({ ...searchFlags, "email": !searchFlags["email"]}))
+        sortDirection = searchFlags["email"];
+      }
+
+     if (searchValueA < searchValueB) {
+      return -1;
+    }
+    if (searchValueA > searchValueB) {
+      return 1;
+    }
+    return 0;
+  },
+  );
+  if (sortDirection) {
+    setSearchResults(sortedSearchResults);
+  }
+  else {
+     setSearchResults(sortedSearchResults.reverse());
+  }
+}
+
+return (
+  <div>
+    <input
+      type="text"
+      placeholder="Search"
+      value={searchTerm}
+      onChange={handleChange}
+    />
     <table className="table table-striped">
       <thead>
         <tr>
           <th scope="col">Photo</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
+          <th scope="col" onClick={() => sortEmployeesBy("name.first")}>Name</th>
+          <th scope="col" onClick={() => sortEmployeesBy("email")}>Email</th>
           <th scope="col">Phone</th>
         </tr>
       </thead>
@@ -73,11 +117,11 @@ function Table() {
               <td>{result.cell}</td>
             </tr>
           )
-        }): null}
+        }) : null}
       </tbody>
     </table>
-    </div>
-  )
+  </div>
+)
 }
 
 export default Table;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 // Export an object with a "search" method that searches for random users for the passed query
 const API = {
   getEmployees: function () {
-    return fetch("https://randomuser.me/api/?results=200&nat=us")
+    return fetch("https://randomuser.me/api/?results=10&nat=us")
   }
 };
 
